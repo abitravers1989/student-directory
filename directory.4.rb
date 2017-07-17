12595414cad8ea541983ba0bce69d3032f4e32ca
abitravers1989 <atravers4007@gmail.com>
2017-07-17 10:39:11 +0100

    First attempt at 4

---
+++ directory.4.rb
@@ -0,0 +1,52 @@
+
+#Array of all the students
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #create an empty Array
+  students = []
+  #gets the first name
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student hash to the Array
+    students << {name: name, cohort: :november}
+    puts "now we have #{students.count} students"
+    #get another name from the user
+    name = gets.chomp
+  end
+  #return the array of students
+  students
+#because i want to be able to use the :names data elsewehere?
+
+end
+
+
+def print_header
+puts "The students of Villains Academy"
+puts "-------------"
+end
+
+
+def print(students)
+     arr = []
+     arr << students
+      while students != arr
+          puts "#{students}"
+          #{students[:name]} (#{students[:cohort]}cohort)"
+          end
+        end
+
+
+
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+# Nothing will happen until we call the methods
+students = input_students
+print_header
+print(students)
+print_footer(students)
